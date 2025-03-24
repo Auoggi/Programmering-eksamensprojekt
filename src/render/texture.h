669aaba0ab665ce5e0117bbf1a22b4e84e9acac2
auoggi <augustoggiahrens@gmail.com>
2025-03-24 22:06:15 +0100

    Implement Texture class

---
+++ src/render/texture.h
@@ -0,0 +1,12 @@
+#include <glad/glad.h>
+
+class Texture {
+public:
+    unsigned int ID;
+    unsigned int width, height;
+
+    Texture();
+
+    void generate(unsigned int width, unsigned int height, unsigned char *data);
+    void bind();
+};
