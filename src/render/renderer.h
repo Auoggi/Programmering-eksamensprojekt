e76c8f2c5e6e0c1ee2974cf60d679ee20edf15f6
auoggi <augustoggiahrens@gmail.com>
2025-03-24 23:13:22 +0100

    Implement Renderer class

---
+++ src/render/renderer.h
@@ -0,0 +1,25 @@
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include "resource_manager.h"
+#include "texture.h"
+#include "shader.h"
+
+class Renderer {
+public:
+    Renderer(Shader &shader);
+    ~Renderer(); // Detructor, is called when class is destroyed / deleted
+
+    // Create a new renderer suitable for rendering simple textures
+    static Renderer* setupRenderer(unsigned int windowWidth, unsigned int windowHeight);
+
+    void drawTexture(Texture &texture, glm::vec2 pos);
+    void drawTexture(Texture &texture, glm::vec2 pos, glm::vec2 size, float rotation);
+private:
+    Shader shader;
+    unsigned int VAO;
+
+    void initRenderData();
+};
