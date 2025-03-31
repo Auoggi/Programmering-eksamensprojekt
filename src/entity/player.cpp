61d5512ad2361f1a4ee67f38228c0d7371c4a8a5
Hveppe <mikkelhvelp@gmail.com>
2025-03-31 13:29:03 +0200

    Rendering player inside player class

--- src/entity/player.cpp
+++ src/entity/player.cpp
@@ -1,6 +1,8 @@
 #include "player.h"
 
-Player::Player() : pos(glm::vec2(100, 100)), speed(7) {}
+Player::Player() : pos(glm::vec2(100, 100)), speed(7) {
+    texture = ResourceManager::loadTexture("assets/textures/ball.png", "player");
+}
 
 void Player::processInput(GLFWwindow *window) {
     glm::vec2 direction = glm::vec2(0, 0);
@@ -26,7 +28,9 @@ void Player::processInput(GLFWwindow *window) {
 }
 
 glm::mat4 Player::getView(int width, int height) {
-    glm::mat4 view = glm::translate(glm::mat4(1.f), glm::vec3(-this->pos.x + width/2, -this->pos.y + height/2, 0));
+    return glm::translate(glm::mat4(1.f), glm::vec3(-this->pos.x + width/2, -this->pos.y + height/2, 0));
+}
 
-    return view;
+void Player::draw(Renderer *renderer, glm::mat4 view) {
+    renderer->drawTexture(this->texture, view, this->pos, glm::vec2(25, 25), 0);
 }
--- src/entity/player.h
+++ src/entity/player.h
@@ -1,12 +1,16 @@
 #ifndef PLAYER_CLASS
 #define PLAYER_CLASS
 
+// Renderer needs to be included first, as glad.h cannot be included after glfw3.h
+#include "../render/renderer.h"
+
 #include <glm/glm.hpp>
 #include <GLFW/glfw3.h>
 #include <glm/gtc/matrix_transform.hpp>
 
 class Player {
 public:
+    Texture texture;
     glm::vec2 pos;
     float speed;
 
@@ -14,6 +18,7 @@ public:
 
     void processInput(GLFWwindow *window);
     glm::mat4 getView(int width, int height);
+    void draw(Renderer *renderer, glm::mat4 view);
 };
 
 #endif
