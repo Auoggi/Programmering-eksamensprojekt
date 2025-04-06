1e20ea051f66478a4ea0a26883e93f8693c04d46
auoggi <augustoggiahrens@gmail.com>
2025-04-06 21:55:54 +0200

    Improve shader symbols generation with shorter naming

--- src/render/shaders/shader_symbols.h
+++ src/render/shaders/shader_symbols.h
@@ -1,6 +1,18 @@
 // THIS FILE IS AUTO-GENERATED, DO NOT EDIT
 
-extern char _binary_src_render_shaders_grid_fragment_glsl_end[];
-extern char _binary_src_render_shaders_grid_fragment_glsl_start[];
-extern char _binary_src_render_shaders_grid_vertex_glsl_end[];
-extern char _binary_src_render_shaders_grid_vertex_glsl_start[];
+namespace shader_symbols {
+	extern char shader_symbols::_binary_src_render_shaders_grid_fragment_glsl_start[];
+	extern char shader_symbols::_binary_src_render_shaders_grid_fragment_glsl_end[];
+}
+const int grid_fragment_glsl_size = shader_symbols::_binary_src_render_shaders_grid_fragment_glsl_end - shader_symbols::_binary_src_render_shaders_grid_fragment_glsl_start;
+const char *grid_fragment_glsl = shader_symbols::_binary_src_render_shaders_grid_fragment_glsl_start;
+#define GRID_FRAGMENT_GLSL grid_fragment_glsl, grid_fragment_glsl_size
+
+namespace shader_symbols {
+	extern char shader_symbols::_binary_src_render_shaders_grid_vertex_glsl_start[];
+	extern char shader_symbols::_binary_src_render_shaders_grid_vertex_glsl_end[];
+}
+const int grid_vertex_glsl_size = shader_symbols::_binary_src_render_shaders_grid_vertex_glsl_end - shader_symbols::_binary_src_render_shaders_grid_vertex_glsl_start;
+const char *grid_vertex_glsl = shader_symbols::_binary_src_render_shaders_grid_vertex_glsl_start;
+#define GRID_VERTEX_GLSL grid_vertex_glsl, grid_vertex_glsl_size
+
