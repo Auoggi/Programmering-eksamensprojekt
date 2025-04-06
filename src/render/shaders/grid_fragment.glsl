4047d6b56ec5bc67d4aff58f354d9f65844657ad
auoggi <augustoggiahrens@gmail.com>
2025-04-06 15:45:51 +0200

    Fix grid not aligning

--- src/render/shaders/grid_fragment.glsl
+++ src/render/shaders/grid_fragment.glsl
@@ -3,14 +3,16 @@ out vec4 FragColor;
 in vec2 fragCoord;
 
 uniform mat4 view;
-uniform float u_tileSize;
+uniform float tileSize;
+uniform float screenHeight;
 
 void main() {
-    vec4 tPos = view * vec4(-gl_FragCoord.x, gl_FragCoord.y, 0, 1);
+    // screenHeight is subtracted from y coordinate because +y is down but y=0 is at the bottom of the screen
+    vec4 tPos = view * vec4(-gl_FragCoord.x, gl_FragCoord.y - screenHeight, 0, 1);
 
     FragColor = mix(
         vec4(0, 0, 0, 0), 
         vec4(1, 1, 1, 1), 
-        step(mod(tPos.x, u_tileSize), 1.0) + step(mod(tPos.y, u_tileSize), 1.0)
+        step(mod(tPos.x, tileSize), 1.0) + step(mod(tPos.y, tileSize), 1.0)
     );
 }
