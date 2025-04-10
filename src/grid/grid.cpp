df60190d5bd916a79426b8af7ee4f810bdb774e8
auoggi <augustoggiahrens@gmail.com>
2025-04-10 08:25:36 +0200

    Added comments to Grid and Entity

--- src/grid/grid.cpp
+++ src/grid/grid.cpp
@@ -2,6 +2,7 @@
 #include "../entity/entity.h" // Import Entity to define Entity class after the forward declaration
 
 Grid::Grid(int tileSize, float screenWidth, float screenHeight) : tileSize(tileSize), shader(ResourceManager::setShader(GRID_VERTEX_GLSL, GRID_FRAGMENT_GLSL, "grid")) {
+    // Define fullscreen VAO, to let the fragment shader draw on the entiry screen
     float vertices[] = {
         0, 0,  
         screenWidth, 0,  
@@ -9,10 +10,13 @@ Grid::Grid(int tileSize, float screenWidth, float screenHeight) : tileSize(tileS
         screenWidth, screenHeight
     };
 
+    
+    // Create VAO and VBO
     GLuint VBO;
     glGenVertexArrays(1, &this->VAO);
     glGenBuffers(1, &VBO);
 
+    // Send VAO and VBO to the gpu, for use in rendering
     glBindVertexArray(this->VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
@@ -21,8 +25,9 @@ Grid::Grid(int tileSize, float screenWidth, float screenHeight) : tileSize(tileS
 }
 
 std::shared_ptr<std::vector<Entity*>> Grid::getEntityList(const glm::ivec2& pos) {
-    if (entityMap.find(pos) == entityMap.end()) {
-        entityMap[pos] = std::make_shared<std::vector<Entity*>>();
+    // Check if entityMap contains pos, returns true if it does not contain pos
+    if(entityMap.find(pos) == entityMap.end()) {
+        entityMap[pos] = std::make_shared<std::vector<Entity*>>(); // Create new vector at pos in entityMap
     }
     return entityMap[pos];
 }
