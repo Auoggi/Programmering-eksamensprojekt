35ab6077bcdfff6467a4b5b7d6ed91c734c829d2
Ag-chr <agc@aktivit.dk>
2025-04-10 19:50:28 +0000

    Made client a class but sending a message doesn't really work now. Will fix later.

--- src/network/UDP_client.cpp
+++ src/network/UDP_client.cpp
@@ -4,29 +4,68 @@
 
 using boost::asio::ip::udp;
 
+class udp_client {
+public:
+    udp_client(boost::asio::io_context& io_context)
+        : socket_(io_context),
+          resolver_(io_context)
+    {
+        socket_.open(udp::v4());
+    }
+
+    void connect(std::string ipv4, std::string port) {
+        receiver_endpoint = *resolver_.resolve(udp::v4(), ipv4, port).begin();
+
+        send_buf.at(0) = 0;
+        socket_.send_to(boost::asio::buffer(send_buf), receiver_endpoint);
+    }
+
+    void send_message(std::string text) {
+        for (int i = 0; i < text.size() || i < send_buf.max_size(); ++i) {
+            send_buf.at(i) = text[i];
+        }
+        //std::cout.write(send_buf.data(), text.size());
+
+        socket_.send_to(boost::asio::buffer(send_buf), receiver_endpoint);
+    }
+
+    std::string listen() {
+        udp::endpoint sender_endpoint;
+        size_t len = socket_.receive_from(boost::asio::buffer(recv_buf), sender_endpoint);
+
+        std::string message;
+        for (int i = 0; i < len; ++i) {
+            message.push_back(recv_buf[i]);
+        }
+        return message;
+    }
+
+private:
+    udp::socket socket_;
+    udp::resolver resolver_;
+    udp::endpoint receiver_endpoint;
+    boost::array<char, 128> send_buf;
+    boost::array<char, 128> recv_buf;
+};
+
+
 int main(int argc, char* argv[]) {
     try {
         if (argc != 2) {
             std::cerr << "Usage: client <host>" << std::endl;
             return 1;
         }
-
         boost::asio::io_context io_context;
 
-        udp::resolver resolver(io_context);
-        udp::endpoint receiver_endpoint = *resolver.resolve(udp::v4(), argv[1], "8080").begin();
-
-        udp::socket socket(io_context);
-        socket.open(udp::v4());
-
-        boost::array<char, 1> send_buf = {{ 0 }};
-        socket.send_to(boost::asio::buffer(send_buf), receiver_endpoint);
-
-        boost::array<char, 128> recv_buf;
-        udp::endpoint sender_endpoint;
-        size_t len = socket.receive_from(boost::asio::buffer(recv_buf), sender_endpoint);
-
-        std::cout.write(recv_buf.data(), len);
+        udp_client udp_client(io_context);
+        udp_client.connect(argv[1], "8080");
+        udp_client.send_message("hi");
+        
+        for (;;)
+        {
+            std::cout << udp_client.listen() << std::endl;
+        }
+        
     }
     catch (std::exception& e) {
         std::cerr << e.what() << std::endl;
