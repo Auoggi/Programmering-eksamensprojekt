798fb34eefbb99c8d6ea3cb2a0c3ce6dae2fae4c
Ag-chr <agc@aktivit.dk>
2025-04-12 14:55:28 +0000

    Added comments to client and server

--- src/network/UDP_server.cpp
+++ src/network/UDP_server.cpp
@@ -11,7 +11,7 @@ using boost::asio::ip::udp;
 
 class UdpServer {
 public:
-    UdpServer(boost::asio::io_context& ioContext)
+    UdpServer(boost::asio::io_context& ioContext) // io_context is used for async
         : socket(ioContext, udp::endpoint(udp::v4(), 8080)),
           timer(ioContext, boost::asio::chrono::seconds(1))
     {
@@ -23,7 +23,7 @@ private:
     void start_receive() {
         socket.async_receive_from(
             boost::asio::buffer(recvBuffer), currentClient,
-            boost::bind(&UdpServer::handle_receive, this,
+            boost::bind(&UdpServer::handle_receive, this, // bind is used to let Boost know to run a callback function (handle_receive) with these arguments
                 boost::asio::placeholders::error,
                 boost::asio::placeholders::bytes_transferred));
     }
@@ -32,15 +32,15 @@ private:
         if (!error) {
             std::string message;
 
-            if (recvBuffer[0] == 0) {
+            if (recvBuffer[0] == 0) { // Client connects to server
                 clients.insert(currentClient);
-                message = std::string("hello\n");
+                message = std::string("hello");
             }
-            else if (recvBuffer[0] == 4) {
+            else if (recvBuffer[0] == 4) { // Client disconnects from server
                 clients.erase(currentClient);
-                message = std::string("goodbye\n");
+                message = std::string("goodbye");
             }
-            else if (clients.count(currentClient)) {                
+            else if (clients.count(currentClient)) { // If none of the above send back what the client sent                
                 recvBuffer.at(bytesTransferred) = '\0';
                 message = recvBuffer.data();
             }
@@ -50,7 +50,8 @@ private:
         }
     }
 
-    void handle_send(boost::shared_ptr<std::string> /*message*/,
+    // A callback function used if something needs to be done after sending a message
+    void handle_send(boost::shared_ptr<std::string> /*message*/, 
         const boost::system::error_code& /*error*/,
         std::size_t /*bytes_transferred*/)
     {
@@ -65,18 +66,19 @@ private:
                         boost::asio::placeholders::bytes_transferred));
     }
 
+    // sends message to all of the connected clients every second
     void messageInInterval() {
         for (udp::endpoint client : clients) {
             send_message("Message in interval of 1 second", client);
         }
 
-        timer.expires_at(timer.expiry() + boost::asio::chrono::seconds(1)); // Will send every second
+        timer.expires_at(timer.expiry() + boost::asio::chrono::seconds(1)); // Starts new timer to one second in the future
         timer.async_wait(boost::bind(&UdpServer::messageInInterval, this));
     }
 
     boost::asio::steady_timer timer;
     udp::socket socket;
-    udp::endpoint currentClient;
+    udp::endpoint currentClient; // client who recently send a packet
     boost::array<char, 128> recvBuffer;
     std::set<udp::endpoint> clients;
 };
