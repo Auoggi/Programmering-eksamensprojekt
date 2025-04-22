ffe56f876cf3d7f34648e52104b1a07e2be71376
auoggi <augustoggiahrens@gmail.com>
2025-04-22 11:42:30 +0200

    Cleanup and rename grid_vertex to fullscreen_vertex

--- src/grid/grid.cpp
+++ src/grid/grid.cpp
@@ -1,7 +1,7 @@
 #include "grid.h"
 #include "../entity/entity.h" // Import Entity to define Entity class after the forward declaration
 
-Grid::Grid(int tileSize, float screenWidth, float screenHeight) : tileSize(tileSize), shader(ResourceManager::setShader(GRID_VERTEX_GLSL, GRID_FRAGMENT_GLSL, "grid")) {
+Grid::Grid(int tileSize, float screenWidth, float screenHeight) : tileSize(tileSize), shader(ResourceManager::setShader(FULLSCREEN_VERTEX_GLSL, GRID_FRAGMENT_GLSL, "grid")) {
     this->shader.use();
     this->shader.setFloat("tileSize", this->tileSize);
     this->shader.setVector2f("screenSize", screenWidth, screenHeight);
