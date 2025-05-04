16af3b3f36f34a1112410d337ef90b69044d6783
Ag-chr <agc@aktivit.dk>
2025-05-04 08:14:35 +0000

    made packets.cpp polymorphic (so it uses inheritance) and changed the packet information to a binary  framing

--- src/network/Packets.cpp
+++ src/network/Packets.cpp
@@ -1,120 +1,129 @@
-
-#include <variant>
 #include <string>
-#include <iostream>
-#include <format>
-#include <sstream>
+#include <memory>
+#include <string_view>
+#include <vector>
 
-enum class PacketType {
+#include <cstdint>
+#include <cstring>
+#include <iostream>
+
+//https://chatgpt.com/share/68163d5f-c924-8003-97de-ff7cd7625af8
+
+class ByteBuffer {
+public:
+    std::vector<char> data;
+    size_t read_offset = 0;
+
+    template<typename T>
+    void put(T value) {
+        static_assert(std::is_trivially_copyable_v<T>, "Only trivial types allowed");
+        char* p = reinterpret_cast<char*>(&value); // gør så at den givne værdi bliver set som en uint8_t pointer i stedet for den give type
+        data.insert(data.end(), p, p + sizeof(T));
+    }
+
+    template<typename T>
+    T read() {
+        static_assert(std::is_trivially_copyable_v<T>, "Only trivial types allowed");
+        if (read_offset + sizeof(T) > data.size()) {
+            throw std::runtime_error("Read past end of buffer");
+        }
+
+        T value;
+        std::memcpy(&value, &data[read_offset], sizeof(T));
+        read_offset += sizeof(T);
+        return value;
+    }
+
+    void reset() { read_offset = 0; }
+
+    const char* raw() const { return data.data(); }
+    size_t size() const { return data.size(); }
+};
+
+enum class PacketType : uint8_t {
     MOVEMENT,
     ATTACK,
     STAMINA,
     HEALTH,
     USE,
 };
+// [ length: 4-byte ][ type: 1-byte ][ payload... ]
+// TODO: lær mere om virtual
+struct Packet {
+    virtual ~Packet() = default;
+    virtual PacketType type() const = 0;
+    virtual std::string serialize() const = 0;
+    static std::unique_ptr<Packet> deserialize(std::string str);
+};
 
-struct packetHeader {
+struct MovementPacket : Packet {
     int id;
-};
+    float x, y;
+    PacketType type() const override { return PacketType::MOVEMENT; }
 
-struct movementPacket : packetHeader {
-    float x,y;
-    float* pos[2] = {&x, &y};
-};
-
-struct attackPacket : packetHeader {
-    int type;
-    float angle;
-};
-
-struct staminaPacket : packetHeader {
-    float stamina;
-};
-
-struct healthPacket : packetHeader {
-    float health;
-};
-
-struct usePacket : packetHeader {
-    std::string use;
-};
-
-using packetType = std::variant<movementPacket, attackPacket, staminaPacket, healthPacket, usePacket>;
-
-
-std::string createPacket(packetType packet) {
-
-    // gets packet header
-    packetHeader header;
-    std::visit([&header](auto&& pkt) {
-        packetHeader& base = pkt;
-        header = base;
-    }, packet);
-
-    std::ostringstream msg;
-    msg << "id=" << header.id << "\n";
-
-    msg << "type=";
-    if (std::holds_alternative<movementPacket>(packet)) {
-        movementPacket movement = std::get<movementPacket>(packet);
-        msg << "movement" << "\n";
-        
-        // payload
-        msg << "\n";
-        msg << movement.x << "\n";
-        msg << movement.y << "\n";
+    MovementPacket(int id_, float x_, float y_) {
+        id = id_;
+        x = x_;
+        y = y_;
     }
-    else if (std::holds_alternative<attackPacket>(packet)) {
-        attackPacket attack = std::get<attackPacket>(packet);
-        msg << "attack" << "\n";
-        
-        // payload
-        msg << "\n";
-        msg << attack.type << "\n";
-        msg << attack.angle << "\n";
-    }
-    else if (std::holds_alternative<staminaPacket>(packet)) {
-        staminaPacket stamina = std::get<staminaPacket>(packet);
-        msg << "stamina" << "\n";
-        
-        // payload
-        msg << "\n";
-        msg << stamina.stamina << "\n";
-    }
-    else if (std::holds_alternative<healthPacket>(packet)) {
-        healthPacket health = std::get<healthPacket>(packet);
-        msg << "health" << "\n";
-        
-        // payload
-        msg << "\n";
-        msg << health.health << "\n";
-    }
-    else if (std::holds_alternative<usePacket>(packet)) {
-        usePacket use = std::get<usePacket>(packet);
-        msg << "use" << "\n";
-        
-        // payload
-        msg << "\n";
-        msg << use.use << "\n";
-    }
-    
-    return msg.str();
-}
 
-std::string msg = "Hello,\n"
-                  "This is line 2\n"
-                  "And this is line 3\n";
+    std::string serialize() const override {
+        ByteBuffer buffer;
+        uint32_t payload_size = sizeof(id) + sizeof(x) + sizeof(y);
 
-packetType parsePacket(std::string) {
+        buffer.put<uint32_t>(payload_size);
+        buffer.put<uint8_t>(static_cast<uint8_t>(type()));
+        buffer.put<int>(id);
+        buffer.put<float>(x);
+        buffer.put<float>(y);
 
+        std::cout << buffer.read<uint32_t>() << std::endl;
+        std::cout << static_cast<uint32_t>(buffer.read<uint8_t>()) << std::endl;
+        std::cout << buffer.read<int>() << std::endl;
+        std::cout << buffer.read<float>() << std::endl;
+        std::cout << buffer.read<float>() << std::endl;
+
+        std::string str(buffer.data.begin(), buffer.data.end());
+        return str;
+    }
+};
+/*
+std::cout << buffer.read<uint32_t>() << std::endl;
+std::cout << static_cast<uint32_t>(buffer.read<uint8_t>()) << std::endl;
+std::cout << buffer.read<int>() << std::endl;
+std::cout << buffer.read<float>() << std::endl;
+std::cout << buffer.read<float>() << std::endl;
+*/
+
+
+std::unique_ptr<Packet> Packet::deserialize(std::string str) {
+    ByteBuffer buffer;
+    buffer.data.assign(str.begin(), str.end());
+
+    uint32_t length = buffer.read<uint32_t>();
+    PacketType type = static_cast<PacketType>(buffer.read<uint8_t>());
+
+    switch (type) {
+        case PacketType::MOVEMENT:
+            int id = buffer.read<int>();
+            float x = buffer.read<float>();
+            float y = buffer.read<float>();
+            return std::make_unique<MovementPacket>(id, x, y);
+        /*default:
+            throw std::invalid_argument("Unknown PacketType");*/
+    };
 }
 
 int main() {
-    movementPacket packet;
-    packet.id = 2;
-    packet.x = 5.5;
-    packet.y = 8.2;
+    MovementPacket packet(2, 5.5, 8.2);
 
-    std::cout << createPacket(packet) << std::endl;
+    std::string packet_string = packet.serialize();
+    std::unique_ptr<Packet> pkt = Packet::deserialize(packet_string);
+
+
+    switch (pkt->type()) {
+        case PacketType::MOVEMENT:
+            std::cout << "Packet is of type MOVEMENT" << std::endl;
+            break;
+    }
 }
-
